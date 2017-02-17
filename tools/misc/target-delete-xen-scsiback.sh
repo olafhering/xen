17dd9a528c72b3675d7203d970198f190a5882a1
Olaf Hering <olaf@aepfle.de>
2017-02-17 10:49:56 +0000

    Scripts to create and delete xen-scsiback nodes in Linux target framework
    Just to make them public, not meant for merging:
    The scripts used during development to create a bunch of SCSI devices in
    dom0 using the Linux target framework. targetcli3 and rtslib3 is used.
    
    A patch is required for python-rtslib:
    http://article.gmane.org/gmane.linux.scsi.target.devel/8146
    
    These scripts are not use by libxl.
    These scripts are meant for testing vscsi= in libxl
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

---
+++ tools/misc/target-delete-xen-scsiback.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+unset LANG
+unset ${!LC_*}
+set -x
+set -e
+
+targetcli --version
+
+configfs=/sys/kernel/config
+target_path=$configfs/target
+
+cd "${target_path}"
+if cd xen-pvscsi
+then
+	for wwn in `ls -d naa.*`
+	do
+		targetcli /xen-pvscsi delete $wwn
+	done
+fi
+cd "${target_path}"
+cd core
+for name in `ls -d pscsi_*/*/wwn`
+do
+	name=${name%/wwn}
+	name=${name##*/}
+	targetcli /backstores/pscsi delete $name
+done
+cd "${target_path}"
+cd loopback
+for wwn in `ls -d naa.*`
+do
+	targetcli /loopback delete $wwn
+done
+cd "${target_path}"
+cd core
+for name in `ls -d fileio_*/*/wwn`
+do
+	name=${name%/wwn}
+	name=${name##*/}
+	targetcli /backstores/fileio delete $name
+done
